958e1f7792b9ec2683c8404016c75af621166149
festsnusa <lukatimof@mail.ru>
2023-08-09 12:33:18 +0700

    feat: add todoList viewer example

---
+++ app/app.vue
@@ -0,0 +1,30 @@
+<template>
+  <img src="./public/todo.jpg" alt="todo">
+  <div>
+    <button @click="fetchTodoList">fetch data</button>
+    <ul>
+      <li v-for="todo in todoList" :key="`todo-id-${todo.id}`">
+        <input type="checkbox" :checked="todo.completed"> {{ todo.title }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { defineNuxtComponent } from '#app'
+
+export default defineNuxtComponent({
+  data: () => ({
+    todoList: []
+  }),
+  methods: {
+    fetchTodoList() {
+      fetch('https://jsonplaceholder.typicode.com/todos/')
+        .then(response => response.json())
+        .then(json => {
+          this.todoList = json
+        })
+    }
+  }
+})
+</script>
