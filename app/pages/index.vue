98f1e2197ab932329fb6b41b693fb10523a04af0
festsnusa <lukatimof@mail.ru>
2023-08-11 12:00:29 +0700

    feat: added Google Fonts

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,16 @@
 <script setup></script>
 
 <template>
+  <Html>
+
+  <Head>
+    <Title>Water Bottle</Title>
+    <Link rel="preconnect" href="https://fonts.googleapis.com" />
+    <Link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <Link href="https://fonts.googleapis.com/css2?family=Qwitcher+Grypen&display=swap" rel="stylesheet" />
+  </Head>
+
+  </Html>
   <NuxtLayout>
     <h1>Home Page</h1>
   </NuxtLayout>
