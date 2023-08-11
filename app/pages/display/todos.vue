31979e1cc7b97c75d847cb8eeb8cf52239f7bd0c
festsnusa <lukatimof@mail.ru>
2023-08-11 12:00:11 +0700

    feat: layout for todo

--- app/pages/display/todos.vue
+++ app/pages/display/todos.vue
@@ -19,25 +19,27 @@ const remainingItems = computed(() => {
 </script>
 
 <template>
-  <div class="section">
-    <h1 class="title">This is Todo Viewer</h1>
-    <NuxtPage v-if="route.params.id" />
-    <BaseDisplay v-else title="Todo Viewer" v-model:itemList="todoList">
-      <template v-slot:hero> </template>
+  <NuxtLayout name="todo">
+    <div class="section">
+      <h1 class="title">This is Todo Viewer</h1>
+      <NuxtPage v-if="route.params.id" />
+      <BaseDisplay v-else title="Todo Viewer" v-model:itemList="todoList">
+        <template v-slot:hero> </template>
 
-      <template v-slot:metrics>
-        {{ completedItems.length }} completed |
-        {{ remainingItems.length }} remaining
-      </template>
-      <template v-slot:items>
-        <li v-for="todo in filteredTodoList" :key="`todo-id-${todo.id}`">
-          <input type="checkbox" :checked="todo.completed"> {{ todo.title }}
-          <NuxtLink :to="`/display/todos/${todo.id}`">{{ todo.title }}</NuxtLink>
-        </li>
-      </template>
-    </BaseDisplay>
+        <template v-slot:metrics>
+          {{ completedItems.length }} completed |
+          {{ remainingItems.length }} remaining
+        </template>
+        <template v-slot:items>
+          <li v-for="todo in filteredTodoList" :key="`todo-id-${todo.id}`">
+            <input type="checkbox" :checked="todo.completed"> {{ todo.title }}
+            <NuxtLink :to="`/display/todos/${todo.id}`">{{ todo.title }}</NuxtLink>
+          </li>
+        </template>
+      </BaseDisplay>
 
-  </div>
+    </div>
+  </NuxtLayout>
 </template>
 
 <style lang="scss"></style>
